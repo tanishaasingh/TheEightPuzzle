b065a77e3a57f71e866053f956ce5739b0a16c64
Tanishaa Singh <tanishaasingh16@gmail.com>
2024-10-30 01:39:53 +0000

    more set up to main

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <vector>
 #include <algorithm> 
 
 using namespace std;
@@ -7,8 +8,8 @@ using namespace std;
 int main(int argc,char* argv[]){
 
     int userpuzzlechoice; 
-    int gamepuzzle[9] = {}; 
-    int puzzle1[] = {1, 2, 3, 4, 0, 5, 7, 8, 6};
+    vector<int> gamepuzzle(9); 
+   
 
     cout << "Welcome to tsing038 / 8 puzzle solver" << endl; 
 
@@ -18,7 +19,7 @@ int main(int argc,char* argv[]){
 
     if (userpuzzlechoice == 1) {         // USER CHOOSES DEFAULT
         
-        copy(puzzle1, puzzle1 + 9, gamepuzzle);    // GAMEPUZZLE now contains the same values as PUZZLE 1
+       gamepuzzle = {1, 2, 3, 4, 0, 5, 7, 8, 6};     // GAMEPUZZLE now contains the default val
         
         cout << "Your puzzle set: ";              // display puzzle set 
             for (int i = 0; i < 9; ++i) {
@@ -27,23 +28,42 @@ int main(int argc,char* argv[]){
         cout << endl;
 
     }
+
     else if (userpuzzlechoice == 2) {    // USER CHOOSES TO MAKE PUZZLE 
-        cout << "test 1" << endl; 
+
+        cout << "Enter your puzzle, use a zero to represent the blank." << endl;
+
+        cout << "Enter the first row, use space or tabs between numbers: ";
+
+            for (int i = 0; i < 3; ++i) {
+            cin >> gamepuzzle[i];    }
+            
+
+        cout << "Enter the second row: ";
+
+            for (int i = 3; i < 6; ++i) {
+            cin >> gamepuzzle[i];    }
+
+        cout << "Enter the third row: ";
+
+            for (int i = 6; i < 9; ++i) {
+            cin >> gamepuzzle[i];   }
+        
+
+        cout << endl; 
+        cout << "Your puzzle set: ";              // display puzzle set 
+            for (int i = 0; i < 9; ++i) {
+                cout << gamepuzzle[i] << " ";
+            }
+        cout << endl;
     }
+
     else {
         cout << "ERROR: WRONG INPUT -- RESTART PROGRAM" << endl; 
         return 0; 
     }
   
 
-   /* Type “1” to use a default puzzle, or “2” to enter your own puzzle.
-2
-Enter your puzzle, use a zero to represent the blank
-Enter the first row, use space or tabs between numbers 1 2 3
-Enter the second row, use space or tabs between numbers 4 8 0
-Enter the third row, use space or tabs between numbers 7 6 5*/
-
-    
     
     
     
